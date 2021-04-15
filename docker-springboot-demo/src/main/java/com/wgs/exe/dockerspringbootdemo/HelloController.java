61af5afbb4c63277aeec52aa7d3ef0ba3a744cd7
wanggenshen <genshen.wang@geely.com>
2021-04-15 10:22:26 +0800

    基于Docker的第一个Spring Boot应用服务

---
+++ docker-springboot-demo/src/main/java/com/wgs/exe/dockerspringbootdemo/HelloController.java
@@ -0,0 +1,15 @@
+package com.wgs.exe.dockerspringbootdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/docker/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, Docker Spring Boot Demo!";
+    }
+}
---
+++ docker-springboot-demo/src/test/java/com/wgs/exe/dockerspringbootdemo/DockerSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgs.exe.dockerspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
